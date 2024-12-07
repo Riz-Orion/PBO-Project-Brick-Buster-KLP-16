bed1661bf73f0351e7babad33184c11e667a782c
F1D022116_CINDY AULIA RAHMANI <148706689+CindyAuliaRahmani@users.noreply.github.com>
2024-12-07 21:39:13 +0800

    Progress 3

--- About.java
+++ About.java
@@ -15,32 +15,52 @@ public class About extends JPanel implements KeyListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+    
         // Background
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
-
+    
         // Title
         g.setColor(Color.white);
         g.setFont(new Font("Serif", Font.BOLD, 40));
-        g.drawString("About", getWidth() / 2 - 100, 100);
-
+        g.drawString("About", getWidth() / 2 - 100, 80);
+    
         // Description
         g.setFont(new Font("Serif", Font.PLAIN, 20));
-        String aboutText = "Brick Breaker is a classic arcade game.\n\n"
-                + "Created as a learning project to demonstrate Java GUI capabilities.\n\n"
-                + "Developed by YourName.";
-        int yPosition = 200;
-        for (String line : aboutText.split("\n")) {
-            g.drawString(line, getWidth() / 2 - 300, yPosition);
-            yPosition += 30;
+        String aboutText = "Brick Buster is a modern take on the classic arcade game.\n" +
+                            " In this game, you take control of a paddle to bounce the ball and destroy all the bricks above.\n" +
+                            " Featuring power-ups like Multi Ball, Extend Paddle, Double Score, and Quick Paddle, the game challenges your skills.\n" +
+                            " With each brick broken, you score points and advance through increasingly challenging levels.\n" +
+                            " This game invites you to test your skills and climb the leaderboard!";
+    
+        FontMetrics fm = g.getFontMetrics();
+        int maxLineWidth = getWidth() - 110;  // Max width for each line (leaving some padding)
+        int yPosition = 150;
+    
+        String[] words = aboutText.split(" ");
+        StringBuilder currentLine = new StringBuilder();
+    
+        for (String word : words) {
+            currentLine.append(word).append(" ");
+            if (fm.stringWidth(currentLine.toString()) > maxLineWidth) {
+                g.drawString(currentLine.toString().trim(), 20, yPosition);
+                yPosition += 30;
+                currentLine.setLength(0);  // Reset the current line
+                currentLine.append(word).append(" ");
+            }
         }
-
+        
+        // Draw the last line if there is any remaining text
+        if (currentLine.length() > 0) {
+            g.drawString(currentLine.toString().trim(), 20, yPosition);
+        }
+    
         // Footer instruction
         g.setFont(new Font("Serif", Font.ITALIC, 20));
         g.setColor(Color.gray);
         g.drawString("Press ESC to return to menu", getWidth() / 2 - 150, getHeight() - 50);
     }
+    
 
     @Override
     public void keyPressed(KeyEvent e) {
