1e0489a3ca399f7fd5796d03cad62c5182a6acb0
F1D022116_CINDY AULIA RAHMANI <148706689+CindyAuliaRahmani@users.noreply.github.com>
2024-12-12 20:07:20 +0800

    Progress 4

--- About.java
+++ About.java
@@ -12,53 +12,52 @@ public class About extends JPanel implements KeyListener {
         addKeyListener(this);
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { 
         super.paintComponent(g);
     
-        // Background
+        // Background Hitam
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
     
-        // Title
+        g.setFont(new Font("Serif", Font.BOLD, 50));
         g.setColor(Color.white);
-        g.setFont(new Font("Serif", Font.BOLD, 40));
-        g.drawString("About", getWidth() / 2 - 100, 80);
+        String title = "About";
+        FontMetrics titleMetrics = g.getFontMetrics(); 
+        int titleWidth = titleMetrics.stringWidth(title); 
+        g.drawString(title, (getWidth() - titleWidth) / 2, 80); 
     
-        // Description
+        g.drawLine(50, 100, getWidth() - 50, 100);
+    
+        g.setColor(new Color(50, 50, 50, 200)); 
+        g.fillRoundRect(50, 120, getWidth() - 100, getHeight() - 200, 25, 25);
+    
+        // Teks Konten
+        g.setColor(Color.white);
         g.setFont(new Font("Serif", Font.PLAIN, 20));
-        String aboutText = "Brick Buster is a modern take on the classic arcade game.\n" +
-                            " In this game, you take control of a paddle to bounce the ball and destroy all the bricks above.\n" +
-                            " Featuring power-ups like Multi Ball, Extend Paddle, Double Score, and Quick Paddle, the game challenges your skills.\n" +
-                            " With each brick broken, you score points and advance through increasingly challenging levels.\n" +
-                            " This game invites you to test your skills and climb the leaderboard!";
+        String aboutText = """
+                Brick Buster is a modern take on the classic arcade game.
+                You control a paddle to bounce the ball and destroy all the bricks.
+                Featuring power-ups like Multi Ball, Extend Paddle, and Quick Paddle.
+                Test your skills, break the bricks, and climb the leaderboard!
+                """;
     
-        FontMetrics fm = g.getFontMetrics();
-        int maxLineWidth = getWidth() - 110;  // Max width for each line (leaving some padding)
-        int yPosition = 150;
-    
-        String[] words = aboutText.split(" ");
-        StringBuilder currentLine = new StringBuilder();
-    
-        for (String word : words) {
-            currentLine.append(word).append(" ");
-            if (fm.stringWidth(currentLine.toString()) > maxLineWidth) {
-                g.drawString(currentLine.toString().trim(), 20, yPosition);
-                yPosition += 30;
-                currentLine.setLength(0);  // Reset the current line
-                currentLine.append(word).append(" ");
-            }
-        }
-        
-        // Draw the last line if there is any remaining text
-        if (currentLine.length() > 0) {
-            g.drawString(currentLine.toString().trim(), 20, yPosition);
+        FontMetrics contentMetrics = g.getFontMetrics(); 
+        int yPosition = 160;
+        int lineSpacing = 30; 
+        for (String line : aboutText.split("\n")) {
+            int lineWidth = contentMetrics.stringWidth(line.trim()); 
+            int xPosition = (getWidth() - lineWidth) / 2; 
+            g.drawString(line.trim(), xPosition, yPosition);
+            yPosition += lineSpacing;
         }
     
-        // Footer instruction
-        g.setFont(new Font("Serif", Font.ITALIC, 20));
-        g.setColor(Color.gray);
-        g.drawString("Press ESC to return to menu", getWidth() / 2 - 150, getHeight() - 50);
+        // Footer
+        g.setFont(new Font("Serif", Font.ITALIC, 20)); 
+        g.setColor(Color.lightGray);
+        String footerText = "Press ESC to return to menu";
+        FontMetrics footerMetrics = g.getFontMetrics();
+        int footerWidth = footerMetrics.stringWidth(footerText); 
+        g.drawString(footerText, (getWidth() - footerWidth) / 2, getHeight() - 50); 
     }
     
 
--- Leaderboard.java
+++ Leaderboard.java
@@ -8,14 +8,28 @@ public class Leaderboard extends JPanel implements KeyListener {
     private List<String> leaderboardData;
     private DatabaseManager dbManager = new DatabaseManager();
 
-
-
     public Leaderboard(MainContainer container) {
         this.container = container;
         setFocusable(true);
         setFocusTraversalKeysEnabled(false);
         addKeyListener(this);
         fetchLeaderboardData();
+        requestFocusInWindow();
+
+        // Key Binding sebagai alternatif
+        getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "exit");
+        getActionMap().put("exit", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                container.showCard("MainMenu");
+            }
+        });
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        requestFocusInWindow();
     }
 
     public void fetchLeaderboardData() {
@@ -26,27 +40,35 @@ public class Leaderboard extends JPanel implements KeyListener {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Background
+        // Background Tetap Hitam
         g.setColor(Color.black);
         g.fillRect(0, 0, getWidth(), getHeight());
 
-        // Title
+        g.setFont(new Font("Serif", Font.BOLD, 50));
         g.setColor(Color.white);
-        g.setFont(new Font("Serif", Font.BOLD, 40));
-        g.drawString("Leaderboard", getWidth() / 2 - 150, 100);
-        
-        // Leaderboard Data (Rank, Name, Level, Score)
+        String title = "Leaderboard";
+        FontMetrics titleMetrics = g.getFontMetrics(); 
+        int titleWidth = titleMetrics.stringWidth(title); 
+        g.drawString(title, (getWidth() - titleWidth) / 2, 80); 
+
+        g.drawLine(50, 100, getWidth() - 50, 100);
+
+        g.setColor(new Color(50, 50, 50, 200)); 
+        g.fillRoundRect(50, 140, getWidth() - 100, getHeight() - 200, 25, 25);
+
+        // Leaderboard Data
         g.setFont(new Font("Serif", Font.PLAIN, 20));
+        g.setColor(Color.white);
         int yOffset = 180;
         for (String entry : leaderboardData) {
             g.drawString(entry, getWidth() / 2 - 200, yOffset);
             yOffset += 40;
         }
 
-        // Footer instruction
+        // Footer
         g.setFont(new Font("Serif", Font.ITALIC, 20));
-        g.setColor(Color.gray);
-        g.drawString("Press ESC to return to menu", getWidth() / 2 - 150, getHeight() - 50);
+        g.setColor(Color.lightGray);
+        g.drawString("Press ESC to return to menu", getWidth() / 2 - 150, getHeight() - 30);
     }
 
     @Override
